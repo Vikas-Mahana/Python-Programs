accd3d3947eb45faa031597279026bb49600e04a
Vikas Mahana <maharanavikas2@gmail.com>
2025-01-22 22:55:33 +0530

    file updated

--- Nested_for_loop.py
+++ Nested_for_loop.py
@@ -14,3 +14,21 @@ def pattern(n):
         print()
 
 pattern
+
+
+"""
+print below pattern.
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1
+"""
+
+def pattern(n):
+    for i in range(1, n+1):
+        for j in range(i, 0, -1):
+            print(j, end=" ")
+        print()
+
+pattern(5)
