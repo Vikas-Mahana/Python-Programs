f3e455468d7a3755380d20d92645c0a0953fb4f6
Vikas Mahana <maharanavikas2@gmail.com>
2025-01-23 23:04:58 +0530

    add a new file

--- Pattern_programs.py
+++ Pattern_programs.py
@@ -87,3 +87,21 @@ def pattern(n: int) -> None:
 
 
 pattern(5)
+
+"""
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+"""
+
+
+def pattern(n: int) -> None:
+    for i in range(1, n + 1):
+        for j in range(n, i - 1, -1):
+            print(j, end=" ")
+        print()
+
+
+pattern(5)
