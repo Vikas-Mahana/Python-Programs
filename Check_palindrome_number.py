f1815b98c2ed0cafee1130ba672fc52f364d6019
Vikas Mahana <maharanavikas2@gmail.com>
2025-01-25 23:44:57 +0530

    cahgnes in file

--- Check_palindrome_number.py
+++ Check_palindrome_number.py
@@ -2,16 +2,16 @@
 write a function to check that it is palindrome or not.
 """
 
-# def checkpalindrome(num: int):
-#     n = num
-#     restult = 0
-#     while n > 0:
-#         last_digit = n % 10
-#         result = (restult*10) + last_digit
-#         n = n//10
-#     if num == restult:
-#         print("It is a palindrome.")
-#     else:
-#         print("It is not a palindrome")
+def checkpalindrome(num: int):
+    n = num
+    restult = 0
+    while n > 0:
+        last_digit = n % 10
+        result = (restult*10) + last_digit
+        n = n//10
+    if num == restult:
+        print("It is a palindrome.")
+    else:
+        print("It is not a palindrome")
 
-# checkpalindrome(121)
+checkpalindrome(121)
