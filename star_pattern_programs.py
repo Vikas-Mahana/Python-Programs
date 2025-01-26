5f6d8db5c99b0d11761c1b591fb8c00e08a11931
Vikas Mahana <maharanavikas2@gmail.com>
2025-01-26 23:35:51 +0530

    Add new program to file

---
+++ star_pattern_programs.py
@@ -0,0 +1,17 @@
+"""
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+"""
+
+def pattern(n):
+    for i in range(1, n+1):
+        for _ in range(1, (n+1)-i):
+            print(" ",end=" ")
+        for _ in range(1, i+1):
+            print("*", end=" ")
+        print()
+    
+pattern(5)
