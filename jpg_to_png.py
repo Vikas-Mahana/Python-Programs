3044e1431d3c5b320f990b8b3f5c1e1d93863898
Vikas Mahana <maharanavikas2@gmail.com>
2026-01-12 23:16:36 +0530

    get imges

--- jpg_to_png.py
+++ jpg_to_png.py
@@ -2,9 +2,11 @@
 
 import sys
 import os
+from pathlib import Path
 from PIL import Image
 
 first_arg = sys.argv[1]
+folder_path = Path('D:\Python-Programs\old_images')
 
 parent_dir = os.path.dirname(first_arg)
 second_arg = "New"
@@ -18,3 +20,7 @@ if os.path.isdir(second_arg_path):
 else:
     os.mkdir(second_arg_path)
     print("Folder New created successfully.")
+
+for img in folder_path.iterdir():
+    image = Image.open(img)
+    print(image)
